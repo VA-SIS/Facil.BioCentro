aaa25636feadc25163ba1c8f2b0fe5662f48aca7
Donisetti Ferreira Cosma <donisetti@msn.com>
2024-04-02 15:32:55 -0400

    Rotas

---
+++ src/Facil.BioCentro.Web/Pages/Cadastro/Servicos/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Facil.BioCentro.Web.Pages.Cadastro.Servicos
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
